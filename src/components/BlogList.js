1e0555ff2bf12765fb4f6c46d69983c1914175c0
Darryl Nicholson <darryl.a.nicholson@gmail.com>
2025-11-07 08:48:03 -0800

    Updates UI and migrates to react-scripts
    Refactors the project to use react-scripts instead of craco.
    
    Improves the blog list UI by adding styling and a placeholder image.
    
    Updates NavBar component padding.

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom";
 const BlogList = ({ blogs, title }) => {
   return (
     <div className="blog-list">
-      <h2>{title}</h2>
+      <h2 className="text-5xl my-8 font-bold uppercase">{title}</h2>
       <div class="grid gap-6 grid-cols-3">
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <Link to={`/blogs/${blog.id}`}>
+          <div class="w-full h-[200px] bg-gray-300 mt-1 mb-3"></div>
             <h2>{blog.title}</h2>
             <p>Written by {blog.author}</p>
           </Link>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav className="w-full bg-blue-500 py-4">
+    <nav className="w-full bg-blue-500 p-4">
       <div className="container mx-auto">
         <div className="flex items-center justify-between">
           <h1 className="text-2xl font-bold text-white">The Dojo Blog</h1>
